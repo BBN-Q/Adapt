761eac3dfb10ac194499d8eabe3cdef8d9a7d116
Graham Rowlands <grahamrow@gmail.com>
2016-05-31 17:20:50 -0400

    Better source structure. Added setup.py for pip installing.

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name='adapt',
+    version='0.1',
+    author='Graham Rowlands',
+    package_dir={'':'src'},
+    packages=[
+        'adapt',
+    ],
+    scripts=[],
+    description='Adaptive meshing for fun and profit.',
+    long_description='Adaptive meshing for fun and profit.',
+
+)
